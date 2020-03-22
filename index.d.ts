f22fc3ac4347907b0e23323d6c86776ada53489c
wen <emailatwen@gmail.com>
2020-03-22 21:12:14 +0800

    增加了声明文件

---
+++ index.d.ts
@@ -0,0 +1,35 @@
+import { ProgressPlugin } from 'webpack'
+
+type Color = 'black' | 'red' | 'green' | 'yellow' | 'blue' | 'magenta' | 
+'cyan' | 'white' | 'gray' | 'grey'
+
+export class WebpackProgessBar extends ProgressPlugin {
+  constructor(options?: {
+    /** 总进度背景色 */
+    incomplete?: {
+      /** 背景颜色 默认 'white' */
+      bg: Color;
+      /** 背景内容 默认' ' */
+      content: string;
+    };
+  
+    /** 当前进度背景色 */
+    complete?: {
+      /** 背景颜色 默认 'green' */
+      bg: Color,
+      /** 背景内容 默认 ' ' */
+      content: string;
+    };
+  
+    /** 进度条字符宽度 默认25 */
+    width?: number;
+  
+    /** 是否每次更新都清除 默认true */
+    clear?: boolean;
+  
+    /** 进度条总进度 默认100 */
+    total?: number;
+  });
+
+  private _init(): void;
+}
